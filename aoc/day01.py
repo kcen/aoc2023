0c5ee6093a7d0bdf59c53bed1f9152cf8dad4759
kcen <kcen@users.noreply.github.com>
2023-12-01 15:56:48 -0800

    reformat day1

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,26 +1,25 @@
 """01: PROBLEM NAME"""
 import aoc.util
-import re
-import string
+from re import findall
+from string import digits
+
 
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.lines = self.input.splitlines()
+        self.lines = input.splitlines()
+        digit_words = [r"\d", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        digit_map = dict(zip(digit_words, digits))
+        self.num_word_pattern = rf"(?=({'|'.join(digit_words)}))"
+        self.word_to_num = lambda word: digit_map.get(word, word)
 
     def part_one(self) -> int:
-        numerics = (re.findall(r'\d', l) for l in self.lines)
-        first_last = (int(i[0]+i[-1]) for i in numerics)
-        res = sum(first_last)
-        return res
+        numerics = (findall(r"\d", l) for l in self.lines)
+        first_last = (int(i[0] + i[-1]) for i in numerics)
+        return sum(first_last)
 
     def part_two(self) -> int:
-        digit_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-        digit_map = dict(zip(digit_words, string.digits))
-        num_word_pattern = fr"(?=({'|'.join(digit_words + [r'\d'])}))"
-        word_to_num = lambda word: digit_map.get(word, word)
-        numerics = (re.findall(num_word_pattern, l) for l in self.lines)
-        first_last = (int(word_to_num(i[0])+word_to_num(i[-1])) for i in numerics)
-        res = sum(first_last)
-        return res
+        numerics = (findall(self.num_word_pattern, l) for l in self.lines)
+        first_last = (int(self.word_to_num(i[0]) + self.word_to_num(i[-1])) for i in numerics)
+        return sum(first_last)

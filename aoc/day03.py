0fbe84c41581959fc1aad58b5f2af1e52ab94bcc
kcen <kcen@users.noreply.github.com>
2023-12-03 15:09:06 -0800

    performance updates

--- aoc/day03.py
+++ aoc/day03.py
@@ -4,7 +4,6 @@
 import aoc.util
 from math import prod
 from re import finditer
-from collections import defaultdict
 from itertools import chain
 
 
@@ -12,8 +11,11 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         width = input.find("\n") + 1
 
-        symbol_positions = {m.start() for m in finditer(r"[^\d\.\n]", input)}
-        parts = defaultdict(list)
+        # Dict[Int, List] input position and empty list for parts
+        parts = dict(
+            (m.start(), list())
+            for m in finditer(r"[^\d\.\n]", input)
+        )
         # star_parts = defaultdict(list)
 
         def neighbors(start, end):
@@ -25,19 +27,19 @@ class Solver(aoc.util.Solver):
 
         for digit_group in finditer(r"\d+", input):
             value = int(digit_group.group())
-            touching = neighbors(digit_group.start(), digit_group.end())
-            part_locations = (pos for pos in touching if pos in symbol_positions)
-            for p in part_locations:
-                parts[p].append(value)
-                # if input[p] == '*':
-                #     star_parts[p].append(value)
+            for p in neighbors(digit_group.start(), digit_group.end()):
+                if p in parts:
+                    parts[p].append(value)
+                    # if input[p] == '*':
+                    #     star_parts[p].append(value)
 
-        self.parts = parts
+        self.part_values = parts.values()
+        #self.parts = parts
         # self.star_parts = star_parts
 
     def part_one(self) -> int:
-        return sum(chain.from_iterable(self.parts.values()))
+        return sum(chain.from_iterable(self.part_values))
 
     def part_two(self) -> int:
         # return sum(prod(p) for p in self.star_parts.values() if len(p) == 2)
-        return sum(prod(p) for p in self.parts.values() if len(p) == 2)
+        return sum(prod(p) for p in self.part_values if len(p) == 2)

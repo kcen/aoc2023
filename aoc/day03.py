cc00dee454b82d58798bdfabd69c822a642356c2
kcen <kcen@users.noreply.github.com>
2023-12-03 03:30:31 -0800

    day 3 cleanup

--- aoc/day03.py
+++ aoc/day03.py
@@ -14,8 +14,7 @@ class Solver(aoc.util.Solver):
         width = input.find("\n")
         schematic = input.replace("\n", "")
 
-        symbols = {divmod(m.start(), width) for m in finditer(r"[^\d\.]", schematic)}
-        stars = {divmod(m.start(), width) for m in finditer(r"\*", schematic)}
+        symbols = {m.start() for m in finditer(r"[^\d\.]", schematic)}
         parts = defaultdict(set)
 
         touching = [-width - 1, -width, -width + 1, -1, +1, width - 1, width, width + 1]
@@ -23,16 +22,14 @@ class Solver(aoc.util.Solver):
         for digit_group in finditer(r"\d+", schematic):
             positions = range(*digit_group.span())
             value = int(digit_group.group())
-            neighbors = {divmod(pos + neighbor, width) for neighbor in touching for pos in positions}
+            neighbors = {pos + neighbor for neighbor in touching for pos in positions}
             for p in neighbors & symbols:
                 parts[p].add(value)
 
         self.parts = parts
-        self.stars = stars
 
     def part_one(self) -> int:
         return sum(sum(p) for p in self.parts.values())
 
     def part_two(self) -> int:
-        star_parts = [self.parts[x] for x in self.stars if len(self.parts[x]) == 2]
-        return sum(prod(p) for p in star_parts)
+        return sum(prod(p) for p in  self.parts.values() if len(p) == 2)

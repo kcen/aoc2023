482b387369cac84ee1f5c11cbce579f7080501e9
kcen <kcen@users.noreply.github.com>
2023-12-08 00:05:54 -0800

    performance tweaks

--- aoc/day08.py
+++ aoc/day08.py
@@ -8,22 +8,22 @@ from itertools import cycle
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         steps, _, *nodes = input.splitlines()
-        self.steps = steps
+        self.steps = [(0,1)[c == "R"] for c in steps]
         self.node_map = {l[:3]: (l[7:10], l[12:15]) for l in nodes}
 
     def steps_to(self, start, end):
         path = cycle(self.steps)
         current = start
         iterations = 0
-        while not current.endswith(end):
-            step = next(path)
-            current = self.node_map[current][step == "R"]
+        for step in path:
+            current = self.node_map[current][step]
             iterations += 1
-        return iterations
+            if current.endswith(end):
+                return iterations
 
     def part_one(self) -> int:
         return self.steps_to("AAA", "ZZZ")
 
     def part_two(self) -> int:
-        steps = [self.steps_to(loc, "Z") for loc in self.node_map.keys() if loc[-1] == "A"]
+        steps = (self.steps_to(loc, "Z") for loc in self.node_map.keys() if loc.endswith("A"))
         return lcm(*steps)

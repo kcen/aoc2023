9e70035d78cf18e614c1a9a75c0d0af6c2bda884
kcen <kcen@users.noreply.github.com>
2023-12-10 23:01:05 -0800

    Formatting

--- aoc/day10.py
+++ aoc/day10.py
@@ -5,13 +5,14 @@ import aoc.util
 from queue import Queue
 
 T = {
-    "|": [ ( 0,-1), ( 0, 1) ],
-    "-": [ (-1, 0), ( 1, 0) ],
-    "L": [ ( 0,-1), ( 1, 0) ],
-    "J": [ ( 0,-1), (-1, 0) ],
-    "7": [ (-1, 0), ( 0, 1) ],
-    "F": [ ( 1, 0), ( 0, 1) ],
+    "|": [(0, -1), (0, 1)],
+    "-": [(-1, 0), (1, 0)],
+    "L": [(0, -1), (1, 0)],
+    "J": [(0, -1), (-1, 0)],
+    "7": [(-1, 0), (0, 1)],
+    "F": [(1, 0), (0, 1)],
 }
+
 
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
@@ -21,39 +22,39 @@ class Solver(aoc.util.Solver):
         y, x = divmod(input.index("S"), w + 1)
 
         q = Queue()
-        
-        for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            c = pipe_map[y+dy][x+dx]
-            for dx2,dy2 in T.get(c,[]):
-                if x == x+dx+dx2 and y == y+dy+dy2:
-                    q.put((1,(x+dx,y+dy)))
 
-        pipe_distance = { (x,y): 0 }
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            c = pipe_map[y + dy][x + dx]
+            for dx2, dy2 in T.get(c, []):
+                if x == x + dx + dx2 and y == y + dy + dy2:
+                    q.put((1, (x + dx, y + dy)))
+
+        pipe_distance = {(x, y): 0}
 
         while not q.empty():
-            d,(x,y) = q.get()
+            d, (x, y) = q.get()
 
-            if (x,y) in pipe_distance:
+            if (x, y) in pipe_distance:
                 continue
 
-            pipe_distance[(x,y)] = d
+            pipe_distance[(x, y)] = d
 
-            for dx,dy in T[pipe_map[y][x]]:
-                q.put((d+1,(x+dx,y+dy)))
-            
+            for dx, dy in T[pipe_map[y][x]]:
+                q.put((d + 1, (x + dx, y + dy)))
+
         self.part1 = max(pipe_distance.values())
         insides = 0
-        for y,line in enumerate(pipe_map):
-            for x,c in enumerate(line):
-                if (x,y) in pipe_distance:
+        for y, line in enumerate(pipe_map):
+            for x, c in enumerate(line):
+                if (x, y) in pipe_distance:
                     continue
-                
+
                 crosses = 0
-                x2,y2 = x,y
+                x2, y2 = x, y
 
                 while x2 < w and y2 < h:
                     c_next = pipe_map[y2][x2]
-                    if (x2,y2) in pipe_distance and c_next != "L" and c_next != "7":
+                    if (x2, y2) in pipe_distance and c_next != "L" and c_next != "7":
                         crosses += 1
                     x2 += 1
                     y2 += 1
--- aoc/day11.py
+++ aoc/day11.py
@@ -4,14 +4,17 @@
 import aoc.util
 from itertools import combinations
 
+
 def enumerate_p(image):
     for row_p, row in enumerate(image):
         for col_p, char in enumerate(row):
             yield (row_p, col_p), char
 
+
 def manhattan(p1, p2):
     Δx, Δy = [p - q for p, q in zip(p1, p2)]
     return abs(Δx) + abs(Δy)
+
 
 def expand_and_solve(galaxies, empty_rows, empty_cols, expansion_factor):
     for r in reversed(empty_rows):
@@ -23,17 +26,15 @@ def expand_and_solve(galaxies, empty_rows, empty_cols, expansion_factor):
             if gc > c:
                 galaxies[i] = (gr, gc + (expansion_factor - 1))
     # Measure distances
-    return sum(
-        manhattan(g1, g2)
-        for g1, g2 in combinations(galaxies, 2)
-    )
+    return sum(manhattan(g1, g2) for g1, g2 in combinations(galaxies, 2))
+
 
 class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         image = input.splitlines()
         num_rows = len(image)
         num_cols = len(image[0])
-        galaxies = [p for p, char in enumerate_p(image) if char == '#']
+        galaxies = [p for p, char in enumerate_p(image) if char == "#"]
         empty_rows = [r for r in range(num_rows) if not any(p[0] == r for p in galaxies)]
         empty_cols = [c for c in range(num_cols) if not any(p[1] == c for p in galaxies)]
         # Expand rows and cols

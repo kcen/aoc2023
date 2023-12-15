cc0a660ae7a0fdce366ffc62419e8c85d095c04e
kcen <kcen@users.noreply.github.com>
2023-12-14 22:37:08 -0800

    Day 15

---
+++ aoc/day15.py
@@ -0,0 +1,48 @@
+"""--- Day 15: Lens Library ---"""
+import aoc.util
+
+
+def hasher(stringy):
+    runner = 0
+    for c in stringy:
+        runner += ord(c)
+        runner = (runner * 17) % 256
+    return runner
+
+
+class Solver(aoc.util.Solver):
+    def __init__(self, input: str):
+        self.input = input.split(",")
+
+    def part_one(self) -> int:
+        return sum(hasher(x) for x in self.input)
+
+    def part_two(self) -> int:
+        boxes = {i: [] for i in range(256)}
+        for command in self.input:
+            if "-" in command:
+                label = command[:-1]
+                box = hasher(label)
+                s = [a for a, _ in boxes[box]]
+                try:
+                    idx = s.index(label)
+                    boxes[box].pop(idx)
+                except ValueError:
+                    pass
+            else:
+                label = command[:-2]
+                i = int(command[-1])
+                box = hasher(label)
+                s = [a for a, _ in boxes[box]]
+                try:
+                    idx = s.index(label)
+                    boxes[box][idx] = (label, int(i))
+                except ValueError:
+                    boxes[box].append((label, int(i)))
+
+        runner = 0
+        for key, box in boxes.items():
+            for i, box_item in enumerate(box):
+                runner += (key + 1) * (i + 1) * box_item[1]
+
+        return runner
